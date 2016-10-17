c39fcae46584750ec0943f77448e92317b81052d
Alex Stout <astout@silvervue.com>
2016-10-17 16:11:51 -0600

    fixes another 'cachResults' typo

--- src/android/com/synconset/CordovaHTTP/CordovaHttp.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttp.java
@@ -96,7 +96,7 @@ public abstract class CordovaHttp {
     }
 
     protected boolean getCacheResults() {
-        return this.cachResults;
+        return this.cacheResults;
     }
     
     protected CallbackContext getCallbackContext() {
